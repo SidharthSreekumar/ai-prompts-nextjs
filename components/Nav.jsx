b78ef5e538b1305da3d58ee990557edc2f26e223
Sidharth Sreekumar <sidarth.sreekumar@gmail.com>
2023-07-04 22:13:05 -0400

    Basic mobile navigation complete

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,9 +6,10 @@ import { useState, useEffect } from 'react';
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
-  const isUserLoggedIn = false;
+  const isUserLoggedIn = true;
 
   const [providers, setProviders] = useState(null);
+  const [toggleDropdown, setToggleDropdown] = useState(false);
 
   useEffect(() => {
     const setProviders = async () => {
@@ -16,8 +17,8 @@ const Nav = () => {
       setProviders(response);
     };
 
-    setProviders();
-  });
+    // ! setProviders();
+  }, []);
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
@@ -60,7 +61,9 @@ const Nav = () => {
                   key={provider.name}
                   onClick={() => signIn(provider.id)}
                   className="black_btn"
-                ></button>;
+                >
+                  Sign In
+                </button>;
               })}
           </>
         )}
@@ -75,7 +78,36 @@ const Nav = () => {
               height={37}
               className="rounded-full"
               alt="profile icon"
+              onClick={() => setToggleDropdown((prev) => !prev)}
             />
+            {toggleDropdown && (
+              <div className="dropdown">
+                <Link
+                  href="/profile"
+                  className="dropdown_link"
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  My Profile
+                </Link>
+                <Link
+                  href="/create-prompt"
+                  className="dropdown_link"
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  Create Prompt
+                </Link>
+                <button
+                  type="button"
+                  className='mt-5 w-full black_btn'
+                  onClick={() => {
+                    setToggleDropdown(false);
+                    signOut();
+                  }}
+                >
+                  Sign Out
+                </button>
+              </div>
+            )}
           </div>
         ) : (
           <>
@@ -86,7 +118,9 @@ const Nav = () => {
                   key={provider.name}
                   onClick={() => signIn(provider.id)}
                   className="black_btn"
-                ></button>;
+                >
+                  Sign In
+                </button>;
               })}
           </>
         )}
